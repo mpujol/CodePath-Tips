2dc86bcf470ca6ce17649608a5fa0e057cb1ddf1
mpujol <mike.pujol88@gmail.com>
2015-08-15 20:33:46 -0700

    Added Animations & Headcount

--- CodePath Tips/ViewController.swift
+++ CodePath Tips/ViewController.swift
@@ -14,11 +14,44 @@ class ViewController: UIViewController {
     @IBOutlet weak var billField: UITextField!
     @IBOutlet weak var totalLabel: UILabel!
     @IBOutlet weak var tipControl: UISegmentedControl!
+    @IBOutlet weak var tipTitleLabel: UILabel!
+    @IBOutlet weak var totalTitleLabel: UILabel!
+    @IBOutlet weak var divisionUIView: UIView!
+    @IBOutlet weak var headCountStepper: UIStepper!
+    @IBOutlet weak var headCountLabel: UILabel!
+    @IBOutlet weak var headCountTitleLabel: UILabel!
 
-    
+
+    var animationSwitch = false
+    var tipPercentages = [0.18, 0.2, 0.22]
+
+
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+
+        //HIDE EVERYTHING!!!! Well.. almost everything.
+
+        self.tipLabel.alpha = 0
+        self.tipTitleLabel.alpha = 0
+        self.totalLabel.alpha = 0
+        self.totalTitleLabel.alpha = 0
+        self.tipControl.alpha = 0
+        self.divisionUIView.alpha = 0
+        self.headCountTitleLabel.alpha = 0
+        self.headCountLabel.alpha = 0
+        self.headCountStepper.alpha = 0
+
+        //Call the keyboard
+        self.billField.becomeFirstResponder()
+
+        //Center the UITextView
+        var centerOfTextField = self.billField.frame
+        var wholeScreen = self.view.frame
+
+        centerOfTextField.origin.y = wholeScreen.height/2
+
+        self.billField.frame = centerOfTextField
 
 
     }
@@ -30,14 +63,81 @@ class ViewController: UIViewController {
 
     @IBAction func onEditingChanged(sender: AnyObject) {
 
-        var tipPercentages = [0.18, 0.2, 0.22]
+        //Animate Up & Fade In Code
+        if billField.text.isEmpty == false && animationSwitch == false {
+
+            UIView.animateWithDuration(1.0,
+                delay: 0.0,
+                options: UIViewAnimationOptions.CurveEaseOut,
+                animations: { () -> Void in
+
+                    var centerOfTextField = self.billField.frame
+                    var halfOfTheScreen = self.view.frame
+
+                    centerOfTextField.origin.y -= (halfOfTheScreen.height / 2.4 )
+
+                    self.billField.frame = centerOfTextField
+
+                    self.animationSwitch = true
+
+                    self.tipLabel.alpha = 1
+                    self.tipTitleLabel.alpha = 1
+                    self.totalLabel.alpha = 1
+                    self.totalTitleLabel.alpha = 1
+                    self.tipControl.alpha = 1
+                    self.divisionUIView.alpha = 1
+                    self.headCountTitleLabel.alpha = 1
+                    self.headCountLabel.alpha = 1
+                    self.headCountStepper.alpha = 1
+
+                }) { (YES) -> Void in
+                    println("Success")
+            }
+            
+        }
+        //Animate Down & Fade Out
+        if billField.text.isEmpty == true  && animationSwitch == true{
+
+            UIView.animateWithDuration(1.0,
+                delay: 0.0,
+                options: UIViewAnimationOptions.CurveEaseOut,
+                animations: { () -> Void in
+
+                    var centerOfTextField = self.billField.frame
+                    var wholeScreen = self.view.frame
+
+                    centerOfTextField.origin.y = wholeScreen.height/2
+
+                    self.billField.frame = centerOfTextField
+
+                    self.animationSwitch = false
+
+                    self.tipLabel.alpha = 0
+                    self.tipTitleLabel.alpha = 0
+                    self.totalLabel.alpha = 0
+                    self.totalTitleLabel.alpha = 0
+                    self.tipControl.alpha = 0
+                    self.divisionUIView.alpha = 0
+                    self.headCountTitleLabel.alpha = 0
+                    self.headCountLabel.alpha = 0
+                    self.headCountStepper.alpha = 0
+
+                }) { (YES) -> Void in
+                    println("Success")
+            }
+        }
+
+        //Compute for the tip
+
         var tipPercentage = tipPercentages[tipControl.selectedSegmentIndex]
 
+        var headCount = headCountStepper.value
 
         var billAmount = (billField.text as NSString).doubleValue;
-        var tip = billAmount * tipPercentage
-        var total = billAmount + tip
+        var tip = (billAmount * tipPercentage) / headCount
+        var total = (billAmount + tip) / headCount
 
+        headCountLabel.text = String(format: "%.0f",headCount)
         tipLabel.text = "$\(tip)"
         totalLabel.text = "$\(total)"
 
@@ -49,5 +149,6 @@ class ViewController: UIViewController {
     @IBAction func onTap(sender: AnyObject) {
         view.endEditing(true)
     }
+
 }
 
